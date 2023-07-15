e392017f29a397aed6c3f70f91855057a176d81a
ivm-2023-01 <ivm-2023-01@ivm-2023-01noMacBook-Pro.local>
2023-07-15 23:58:23 +0900

    App js -> jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App_Header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
