61308831285f7b1700ee789e903d39e3355a47d5
inuit0215 <takumi.inui.gd@nttdocomo.com>
2023-07-28 23:03:51 +0900

    Amplify

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -60,10 +60,10 @@ export default function MainPage() {
       <div key={index}>
         <BlogCard
           id={blog.id}
-          title={decodeURI(blog.title)}
-          body={decodeURI(blog.body)}
+          title={blog.title}
+          body={blog.body}
           createdAt={blog.createdAt}
-          description={decodeURI(blog.description)}
+          description={blog.description}
         />
       </div>
     );
